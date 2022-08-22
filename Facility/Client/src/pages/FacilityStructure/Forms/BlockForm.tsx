b6e7fd26ce399d4c73e51c8910efa0e6979f85e3
merve nur yazanel <mervenuryazanel@gmail.com>
2022-08-22 09:01:59 +0300

    block form convertion to react-hook-form

--- Facility/Client/src/pages/FacilityStructure/Forms/BlockForm.tsx
+++ Facility/Client/src/pages/FacilityStructure/Forms/BlockForm.tsx
@@ -4,7 +4,8 @@ import { InputText } from "primereact/inputtext";
 import { Chips } from 'primereact/chips';
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { useAppSelector } from '../../../app/hook';
-
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import FacilityStructureService from "../../../services/facilitystructure";
 
 interface Params {
@@ -43,6 +44,15 @@ interface Node {
     className?: string;
 }
 
+const schema = yup.object({
+
+    // elevation: yup.number().moreThan(-1).notRequired(),
+    // height: yup.number().moreThan(-1).notRequired(),
+    name: yup.string().required("This area is required.").min(2, "This area is accept min 2 characters."),
+
+});
+
+
 const BlockForm = ({
     selectedFacilityType,
     submitted,
@@ -57,15 +67,20 @@ const BlockForm = ({
     setSelectedFacilityType
 }: Params) => {
 
-    const [name, setName] = useState<string>("");
-    const [tag, setTag] = useState<string[]>([]);
-    const [description, setDescription] = useState<string>("");
-    const [projectName, setProjectName] = useState<string>("");
+    const [data, setData] = useState<any>();
+    const { register, handleSubmit, watch, formState: { errors }, control } = useForm({
+        defaultValues: {
+            ...data,
+            tag:data?.tag
+        },
+        resolver: yupResolver(schema)
+    });
+
     const {toast} = useAppSelector(state => state.toast);
 
     useEffect(() => {
         if (submitted) {
-            onSubmit();
+            handleSubmit(onSubmit)();
         }
         setSubmitted(false);
 
@@ -81,10 +96,7 @@ const BlockForm = ({
     const getNodeInfoForUpdate = (selectedNodeKey: string) => {
         FacilityStructureService.nodeInfo(selectedNodeKey)
             .then((res) => {
-                setName(res.data.properties.name || "");
-                setTag(res.data.properties.tag || []);
-                setDescription(res.data.properties.description || "");
-                setProjectName(res.data.properties.projectName || "");
+                setData(res.data.properties);
             })
             .catch((err) => {
                 toast.current.show({
@@ -97,16 +109,16 @@ const BlockForm = ({
     }
 
 
-    const onSubmit = () => {
-
+    const onSubmit = (data:any) => {
+        console.log("asasdasd", data);
         if (editDia === false) {
             let newNode: any = {};
 
             newNode = {
-                name: name,
-                tag: tag,
-                description: description,
-                projectName: projectName,
+                name: data?.name,
+                tag: data?.tag,
+                description: data?.description,
+                projectName: data?.projectName,
                 nodeType: selectedFacilityType,
             };
 
@@ -151,10 +163,10 @@ const BlockForm = ({
 
             let updateNode: any = {};
             updateNode = {
-                name: name,
-                tag: tag,
-                description: description,
-                projectName: projectName,
+                name: data?.name,
+                tag: data?.tag,
+                description: data?.description,
+                projectName: data?.projectName,
                 nodeType: selectedFacilityType,
             };
 
@@ -183,41 +195,77 @@ const BlockForm = ({
     };
 
     return (
-        <div>
+        <form>
             <div className="field">
                 <h5 style={{ marginBottom: "0.5em" }}>Name</h5>
                 <InputText
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}
+                    // value={name}
+                    // onChange={(event) => setName(event.target.value)}
+                    autoComplete="off"
+                    {...register("name")}
                     style={{ width: '100%' }}
+                    defaultValue={data?.name || ""}
                 />
             </div>
+            <p style={{ color: "red" }}>{errors.name?.message}</p>
+
             <div className="field structureChips">
+                <h5 style={{ marginBottom: "0.5em" }}>Tag</h5>
+                <Controller
+                    defaultValue={data?.tag || []}
+                    name="tag"
+                    control={control}
+                    render={({ field }) => (
+                        <Chips
+                            value={field.value}
+                            onChange={(e) => {
+                                // console.log("field value: ", e.value);
+                                console.log("data tag:", data?.tag);
+                                field.onChange(e.value)
+                            }}
+                            style={{ width: "100%" }}
+                        />
+                    )}
+                />
+            </div>
+            {/* <div className="field structureChips">
                 <h5 style={{ marginBottom: "0.5em" }}>Tag</h5>
                 <Chips
                     value={tag}
                     onChange={(e) => setTag(e.value)}
                     style={{ width: "100%" }}
                 />
-            </div>
-            <div className="field">
+            </div> */}
+            {/* <div className="field">
                 <h5 style={{ marginBottom: "0.5em" }}>Description</h5>
                 <InputText
                     value={description}
                     onChange={(event) => setDescription(event.target.value)}
                     style={{ width: '100%' }}
                 />
+            </div> */}
+            <div className="field">
+                <h5 style={{ marginBottom: "0.5em" }}>Description</h5>
+                <InputText
+                    autoComplete="off"
+                    defaultValue={data?.description || ""}
+                    {...register("description")}
+                    style={{ width: "100%" }}
+                />
             </div>
             <div className="field">
                 <h5 style={{ marginBottom: "0.5em" }}>Project Name</h5>
                 <InputText
-                    value={projectName}
-                    onChange={(event) => setProjectName(event.target.value)}
-                    style={{ width: '100%' }}
+                    autoComplete="off"
+                    defaultValue={data?.projectName || ""}
+                    {...register("projectName")}
+                    style={{ width: "100%" }}
                 />
             </div>
+            <p style={{ color: "red" }}>{errors.projectName?.message}</p>
 
-        </div>
+
+        </form>
     );
 };
 
