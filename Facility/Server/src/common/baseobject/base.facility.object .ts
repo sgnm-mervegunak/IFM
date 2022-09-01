d0ac1411fba3d10de2813abca521ec21d4ea33ea
Atamer Atalay <atamer.atalay@signumtte.com>
2022-09-01 11:04:34 +0300

    .

--- Facility/Server/src/common/baseobject/base.facility.object .ts
+++ Facility/Server/src/common/baseobject/base.facility.object .ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export class BaseFacilityObject {
   key: string = generateUuid();
-  createdBy: string = 'atamer.atalay@signumtte.com'; //kullanıcı session dan alınacak 
+  createdBy: string = 'ahmet.bozkurt@signumtte.com'; //kullanıcı session dan alınacak 
   createdOn = moment().format('YYYY-MM-DD HH:mm:ss'); 
   isActive = true;
   isDeleted = false;
