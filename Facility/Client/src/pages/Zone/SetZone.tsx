e82cbaaf8ac45ff1806e510886be08a6e12881cc
sgnm-mustafa <mustafa.durmaz@signumtte.com>
2022-09-09 15:34:10 +0300

    Excel import edit

--- Facility/Client/src/pages/Zone/SetZone.tsx
+++ Facility/Client/src/pages/Zone/SetZone.tsx
@@ -116,6 +116,8 @@ const SetZone = () => {
   const [nodeKeys, setNodeKeys] = useState<string[]>([]);
   const [classificationSpace, setClassificationSpace] = useState<Node[]>([]);
   const [classificationStatus, setclassificationStatus] = useState<Node[]>([]);
+  const [codeCategory, setCodeCategory] = useState("");
+  const [codeStatus, setCodeStatus] = useState("");
   const [formTypeId, setFormTypeId] = useState<any>(undefined);
   const [labels, setLabels] = useState<string[]>([]);
   const [isActive, setIsActive] = useState<boolean>(true);
@@ -236,7 +238,7 @@ const SetZone = () => {
   //             life: 2000,
   //           });
   //         });
-      
+
   //     },
   //   },
   // ];
@@ -285,7 +287,7 @@ const SetZone = () => {
   const { register, handleSubmit, watch, formState: { errors }, control, reset, formState, formState: { isSubmitSuccessful } } = useForm<ZoneInterface>({
     resolver: yupResolver(schema)
   })
- 
+
   useEffect(() => {
     watch((value, { name, type }) => console.log(value, name, type));
   }, [watch])
@@ -293,9 +295,9 @@ const SetZone = () => {
 
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
-      reset({...createZone});
+      reset({ ...createZone });
     } else {
-      
+
     }
   }, [formState, createZone, reset]);
 
@@ -315,10 +317,11 @@ const SetZone = () => {
     }
   };
 
-  const addItem = (createZone:any) => {
+  const addItem = (createZone: any) => {
     let newNode: any = {};
     newNode = {
       ...createZone,
+      category:codeCategory,
       spaceNames: `${selectedKeysName.toString().replaceAll(",", ", ")}` || "",
       nodeKeys: selectedKeys || [],
       credatedBy: "",
@@ -409,7 +412,7 @@ const SetZone = () => {
 
   const onChange = (e: any) => {
     try {
-      
+
       setCreateZone(prev => ({
         ...prev,
         [e.target?.name]: e.target.value
@@ -538,7 +541,7 @@ const SetZone = () => {
 
   return (
 
-   
+
     <div className="container">
 
       {/* {
@@ -598,9 +601,13 @@ const SetZone = () => {
               render={({ field }) => (
                 <TreeSelect
                   value={field.value}
-                  options={classificationStatus}
+                  options={classificationSpace}
                   onChange={(e) => {
-                    field.onChange({ target: { name: "category", value: e.value } })
+                    ClassificationsService.nodeInfo(e.value as string)
+                      .then((res) => {
+                        field.onChange(e.value)
+                        setCodeCategory(res.data.properties.code || "");
+                      })
                   }}
                   filter
                   placeholder="Select Type"
@@ -747,13 +754,13 @@ const SetZone = () => {
           filterBy="name,code"
           filterPlaceholder="Search"
           selectionMode="checkbox"
-          onSelect={(e:any)=>{
-            setSelectedKeysName(prev=>([...prev,e.node.name]))
-            
+          onSelect={(e: any) => {
+            setSelectedKeysName(prev => ([...prev, e.node.name]))
+
           }}
-          onUnselect={(e:any)=>{
-            setSelectedKeysName(prev=>prev.filter(item=>item!==e.node.name))
-            
+          onUnselect={(e: any) => {
+            setSelectedKeysName(prev => prev.filter(item => item !== e.node.name))
+
           }}
           onSelectionChange={(event: any) => {
             console.log(event);
