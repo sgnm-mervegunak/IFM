05f8a08a2779bcb5540f8514e56af44c35643171
Atamer Atalay <atamer.atalay@signumtte.com>
2022-09-28 14:23:56 +0300

    added classname property to asset system entity

--- Asset/Server/src/systems/entities/systems.entity.ts
+++ Asset/Server/src/systems/entities/systems.entity.ts
@@ -1,6 +1,7 @@
 import { BaseAssetObject } from 'src/common/baseobject/base.asset.object';
 
 export class System extends BaseAssetObject{
+    className: string = System.name;
     name: string;   //required
     description: string; //optional
     images: string;  //optional
