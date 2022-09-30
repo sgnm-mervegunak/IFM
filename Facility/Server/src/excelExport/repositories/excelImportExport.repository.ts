e829e850e818d068c873028b6eb11b9d53370d7a
Ahmet Berkay Bozkurt <ahmet.bozkurt@signumtte.com>
2022-09-30 09:13:00 +0300

    asset classsification updated

--- Facility/Server/src/excelExport/repositories/excelImportExport.repository.ts
+++ Facility/Server/src/excelExport/repositories/excelImportExport.repository.ts
@@ -42,7 +42,7 @@ export class ExcelImportExportRepository implements ExcelImportExportInterface<a
       if(data.value.parent_of[0] ==undefined || (data.value.parent_of[0]?.nodeType=="Floor" && typeof data.value.parent_of[0].parent_of=="undefined") ||(data.value.parent_of[0]?.nodeType=="Block" && (typeof data.value.parent_of[0].parent_of=="undefined" ||typeof data.value.parent_of[0].parent_of[0].parent_of=="undefined"))){
         throw new HttpException(there_are_no_spaces_object(),404);
       }
-      //else {
+      else {
         if(data.value.parent_of[0]?.parent_of[0]?.parent_of==undefined){
           for (let index = 0; index < data.value.parent_of?.length; index++) {
         
@@ -144,7 +144,7 @@ export class ExcelImportExportRepository implements ExcelImportExportInterface<a
         }
         console.log(jsonData,"2");
         return jsonData;
-      //}
+      }
       
      
       
