8e776d5204ab2d4dcfecc779264e8b65e3659707
sgnm-haydar <haydar.urdogan@signumtte.com>
2022-09-30 15:17:49 +0300

    main ts port updated

--- Asset/Server/src/main.ts
+++ Asset/Server/src/main.ts
@@ -60,7 +60,7 @@ async function bootstrap() {
     );
     app.enableCors();
     await app.startAllMicroservices();
-    await app.listen(3014);
+    await app.listen( configService.get('ASSET_APP_PORT'));
   } catch (error) {
     console.log(error);
   }
