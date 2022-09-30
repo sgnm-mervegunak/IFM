29712d505dc5e10a60865795db4d1ff5457e38a3
Atamer Atalay <atamer.atalay@signumtte.com>
2022-09-30 12:51:10 +0300

    .

--- Asset/Server/src/common/class/node.relation.dealer.ts
+++ Asset/Server/src/common/class/node.relation.dealer.ts
@@ -40,35 +40,23 @@ export class NodeRelationHandler {
   }
 
   async  manageNodesRelations(categoriesArr: object[], newCategoriesArr: object[], relationArr: string[], _root_idArr: string[])   {
-
-    for (let j = 0; j<categoriesArr.length; j++) {
-      let categories = categoriesArr[j];
-      let newCategories = newCategoriesArr[j];
-      let relation = relationArr[j];
-      let _root_id = _root_idArr[j];
-
-      if (categories && categories['length'] > 0) {
-        if (categories[0]['_fields'][1]['properties'].code != newCategories[0]['_fields'][1]['properties'].code) {
-          for (let i = 0; i < categories['length']; i++) {
-            await this.neo4jService.deleteRelationByIdAndRelationNameWithoutFilters(
-              +_root_id,
-              categories[i]['_fields'][1].identity.low,
-              relation,
-              RelationDirection.RIGHT
-            );
-          }
-          for (let i = 0; i < newCategories['length']; i++) {
-            await this.neo4jService.addRelationByIdAndRelationNameWithFilters(
-              +_root_id,
-              { isDeleted: false },
-              newCategories[i]['_fields'][1].identity.low,
-              { isDeleted: false },
-              relation,
-              RelationDirection.RIGHT
-            );
-          }
-        }
-      } else {
+  
+    if (categoriesArr && categoriesArr.length>0) {
+      for (let j = 0; j<categoriesArr.length; j++) {
+        let categories = categoriesArr[j];
+        let newCategories = newCategoriesArr[j];
+        let relation = relationArr[j];
+        let _root_id = _root_idArr[j];
+      if (categories[0]['_fields'][1]['properties'].code != newCategories[0]['_fields'][1]['properties'].code) {
+        for (let i = 0; i < categories['length']; i++) {
+          await this.neo4jService.deleteRelationByIdAndRelationNameWithoutFilters(
+            +_root_id,
+            categories[i]['_fields'][1].identity.low,
+            relation,
+            RelationDirection.RIGHT
+          );
+         }
+        
         for (let i = 0; i < newCategories['length']; i++) {
           await this.neo4jService.addRelationByIdAndRelationNameWithFilters(
             +_root_id,
@@ -78,10 +66,27 @@ export class NodeRelationHandler {
             relation,
             RelationDirection.RIGHT
           );
-        }
+         }
+       }
       }
-
-    }
+     }
+    else {
+      for (let j = 0; j<newCategoriesArr.length; j++) { 
+        let newCategories = newCategoriesArr[j];
+        let relation = relationArr[j];
+        let _root_id = _root_idArr[j];
+       for (let i = 0; i < newCategories['length']; i++) {
+        await this.neo4jService.addRelationByIdAndRelationNameWithFilters(
+          +_root_id,
+          { isDeleted: false },
+          newCategories[i]['_fields'][1].identity.low,
+          { isDeleted: false },
+          relation,
+          RelationDirection.RIGHT
+        );
+      }
+     }
+    } 
   }
   async  deleteNodesRelations(categoriesArr: object[], relationArr: string[], _root_idArr: string[])   {
 
