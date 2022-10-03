ca22507dbd68b4ae9b224a19c7238f0d452f4cf5
OmBayus <omerbayramcavus@gmail.com>
2022-10-03 16:26:57 +0300

    Refactor loadNode function and rollback function on ClassificationAdminLazy

--- Facility/Client/src/keycloak.ts
+++ Facility/Client/src/keycloak.ts
@@ -1,11 +1,11 @@
-import Keycloak from 'keycloak-js';
+import Keycloak from "keycloak-js";
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
-  url: process.env.REACT_APP_API_KEYCLOAK + '/auth',
-  realm: process.env.REACT_APP_KEYCLOAK_REALM,
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENTID,
+  url: process.env.REACT_APP_API_KEYCLOAK + "/auth",
+  realm: process.env.REACT_APP_KEYCLOAK_REALM || "",
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENTID || "",
 });
 
 // let initOptions = {
