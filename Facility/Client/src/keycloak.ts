4dd18b4e26bdb928a21fcd26f4efd98e0640d2df
sgnm-haydar <haydar.urdogan@signumtte.com>
2022-10-03 16:13:02 +0300

    client updaetd

--- Facility/Client/src/keycloak.ts
+++ Facility/Client/src/keycloak.ts
@@ -4,8 +4,8 @@ import Keycloak from 'keycloak-js';
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
   url: process.env.REACT_APP_API_KEYCLOAK + '/auth',
-  realm: process.env.REACT_APP_KEYCLOAK_REALM,
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENTID,
+  realm: process.env.REACT_APP_KEYCLOAK_REALM as string,
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENTID as string,
 });
 
 // let initOptions = {
