effc71a0bddf9aef70befcf440c99adcbf8d1ab4
sgnm-mustafa <mustafa.durmaz@signumtte.com>
2022-10-05 10:44:41 +0300

    Structure create lazyload

--- Facility/Client/src/services/facilitystructurelazy.ts
+++ Facility/Client/src/services/facilitystructurelazy.ts
@@ -12,12 +12,17 @@ const lazyLoadByKey = (key: string) => {
 
 const loadStructureWithPath = async (path: string[]) => {
     return axios.post(url + "/lazyloading/path", { path });
-  };
+};
+
+const loadStructureWithPathByKey = async (path: string[]) => {
+    return axios.post(url + "/lazyloading/pathByKey", { path,label:"" });
+};
 
 const service = {
     findAll,
     lazyLoadByKey,
-    loadStructureWithPath
+    loadStructureWithPath,
+    loadStructureWithPathByKey
 };
 
 export default service;
