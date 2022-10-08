6d872919eae916aed56611179dfc4bd296a72d8c
Atamer Atalay <atamer.atalay@signumtte.com>
2022-10-09 00:05:57 +0300

    System / component create end point

--- Asset/Server/src/systems/systems.module.ts
+++ Asset/Server/src/systems/systems.module.ts
@@ -5,19 +5,22 @@ import { LazyLoadingRepository } from 'src/common/class/lazyLoading.dealer';
 import { NodeRelationHandler } from 'src/common/class/node.relation.dealer';
 import { VirtualNodeHandler } from 'src/common/class/virtual.node.dealer';
 import { RepositoryEnums } from 'src/common/const/repository.enum';
+import { SystemComponentRelationController } from './controllers/component.relation.controller';
 import { StructureLazyLoadingController } from './controllers/system.lazyloading.controller';
 import { SystemsController } from './controllers/systems.controller';
+import { SystemComponentRepository } from './repositories/component.repository';
 import { SystemLazyLoadingRepository } from './repositories/system.lazyloading.repository';
 import { SystemsRepository } from './repositories/systems.repository';
+import { SystemComponentService } from './services/component.relation.service';
 import { SystemLazyLoadingService } from './services/system.lazyloading.service';
 import { SystemsService } from './services/systems.service';
 
 
 @Module({
   imports: [HttpModule],
-  controllers: [SystemsController, StructureLazyLoadingController],
+  controllers: [SystemsController, StructureLazyLoadingController, SystemComponentRelationController],
   providers: [
-    SystemsService, SystemLazyLoadingService,
+    SystemsService, SystemLazyLoadingService, SystemComponentService,
     {
       provide: RepositoryEnums.SYSTEMS,
       useClass: SystemsRepository,
@@ -41,6 +44,10 @@ import { SystemsService } from './services/systems.service';
     {
       provide: LazyLoadingRepository,
       useClass: LazyLoadingRepository,
+    },
+    {
+      provide: RepositoryEnums.SYSTEM_COMPONENT,
+      useClass: SystemComponentRepository,
     },
   ],
   exports: [SystemsService],
