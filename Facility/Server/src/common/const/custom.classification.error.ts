d27d935d6d0a38986676ebb74feeb00bb289a9c8
Ahmet Berkay Bozkurt <ahmet.bozkurt@signumtte.com>
2022-10-20 17:52:12 +0300

    error functions updated

--- Facility/Server/src/common/const/custom.classification.error.ts
+++ Facility/Server/src/common/const/custom.classification.error.ts
@@ -30,7 +30,7 @@ import { I18NEnums } from "./i18n.enum";
   export function space_has_already_relation() {
     throw new HttpException({ key: I18NEnums.SPACE_HAS_ALREADY_RELATION ,code:CustomClassificationError.SPACE_HAS_ALREADY_RELATION}, HttpStatus.BAD_REQUEST);
   }
-  
+
   export function contact_already_exist(name) {
     throw new HttpException({ key: I18NEnums.CONTACT_ALREADY_EXIST ,code:CustomClassificationError.CONTACT_ALREADY_EXIST,args:{name}}, HttpStatus.BAD_REQUEST);
   }
@@ -51,16 +51,16 @@ import { I18NEnums } from "./i18n.enum";
     throw new HttpException({ key: I18NEnums.THERE_ARE_NO_ZONES,code:CustomClassificationError.THERE_ARE_NO_ZONES}, HttpStatus.NOT_FOUND);
   }
 
-  export function type_already_exists() {
-    throw new HttpException({ key: I18NEnums.TYPE_ALREADY_EXISTS,code:CustomClassificationError.TYPE_ALREADY_EXISTS}, HttpStatus.NOT_FOUND);
+  export function type_already_exists(name) {
+    throw new HttpException({ key: I18NEnums.TYPE_ALREADY_EXISTS,code:CustomClassificationError.TYPE_ALREADY_EXISTS,args:{name}}, HttpStatus.NOT_FOUND);
   }
 
-  export function component_already_exists() {
-    throw new HttpException({ key: I18NEnums.COMPONENT_ALREADY_EXISTS,code:CustomClassificationError.COMPONENT_ALREADY_EXISTS}, HttpStatus.NOT_FOUND);
+  export function component_already_exists(name) {
+    throw new HttpException({ key: I18NEnums.COMPONENT_ALREADY_EXISTS,code:CustomClassificationError.COMPONENT_ALREADY_EXISTS,args:{name}}, HttpStatus.NOT_FOUND);
   }
 
-  export function component_already_exist_inside_a_system() {
-    throw new HttpException({ key: I18NEnums.COMPONENT_ALREADY_EXIST_INSIDE_A_SYSTEM,code:CustomClassificationError.COMPONENT_ALREADY_EXIST_INSIDE_A_SYSTEM}, HttpStatus.NOT_FOUND);
+  export function component_already_exist_inside_a_system(name) {
+    throw new HttpException({ key: I18NEnums.COMPONENT_ALREADY_EXIST_INSIDE_A_SYSTEM,code:CustomClassificationError.COMPONENT_ALREADY_EXIST_INSIDE_A_SYSTEM,args:{name}}, HttpStatus.NOT_FOUND);
   }
 
 
